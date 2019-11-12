6b619df8f625847931d370b9d144918cae13e76b
simsimjae <simsimjae@naver.com>
2019-11-12 19:15:50 +0900

    리소스 경로 맞추기

--- webpack.config.js
+++ webpack.config.js
@@ -4,82 +4,103 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('extract-css-chunks-webpack-plugin');
 
-module.exports = {
-  mode: 'development',
-  entry: {
-    app: path.resolve(__dirname, './src/index.js'),
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js',
-    chunkFilename: '[name]-chunk.js',
-    publicPath: '/',
-  },
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 9000,
-    watchContentBase: true,
-  },
-  devtool: 'inline-source-map',
-  module: {
-    rules: [
-      {
-        test: /\.(html)$/,
-        include: path.join(__dirname, 'src/'),
-        use: [
-          {
-            loader: 'html-loader',
-            options: {
-              interpolate: true,
-              removeComments: true,
-              collapseWhitespace: true
+module.exports = (env, options) => {
+  let config = {
+    mode: 'development',
+    entry: {
+      app: path.resolve(__dirname, './src/index.js'),
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist/nuguya/resources/'),
+      filename: '[name].js',
+      chunkFilename: '[name]-chunk.js',
+      publicPath: '/nuguya/resources/',
+    },
+    devServer: {
+      contentBase: path.join(__dirname, 'dist/nuguya/resources/'),
+      compress: true,
+      port: 9000,
+      watchContentBase: true,
+    },
+    devtool: 'inline-source-map',
+    module: {
+      rules: [
+        {
+          test: /\.(html)$/,
+          include: path.join(__dirname, 'src/'),
+          use: [
+            {
+              loader: 'html-loader',
+              options: {
+                interpolate: true,
+                removeComments: true,
+                collapseWhitespace: true
+              },
             },
-          },
-        ]
-      },
-      {
-        test: /\.js$/,
-        exclude: __dirname + 'node_modules',
-        use: {
-          loader: 'babel-loader',
+          ]
+        },
+        {
+          test: /\.js$/,
+          exclude: __dirname + 'node_modules',
+          use: {
+            loader: 'babel-loader',
+          }
+        },
+        {
+          test: /\.(sa|sc|c)ss$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                hot: true,
+                reloadAll: true, // fallback reload
+              },
+            },
+            'css-loader',
+            //'postcss-loader',
+            'sass-loader',
+          ],
         }
-      },
-      {
-        test: /\.(sa|sc|c)ss$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              hot: true,
-              reloadAll: true, // fallback reload
-            },
-          },
-          'css-loader',
-          //'postcss-loader',
-          'sass-loader',
-        ],
-      }     
-    ]
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      filename: 'home.html',
-      template: './src/home.html',
-    }),
-    new HtmlWebpackPlugin({
-      filename: 'detail.html',
-      template: './src/detail.html',
-    }),
-    new MiniCssExtractPlugin({
-      filename: '[name].css',
-      chunkFilename: '[id].css',
-    }),
-  ],
-  optimization: {},
-  resolve: {
-    modules: ['node_modules'],
-    extensions: ['.js', '.json', '.jsx', '.css'],
-  },
+      ]
+    },
+    plugins: [
+      new CleanWebpackPlugin({
+        cleanOnceBeforeBuildPatterns: ['/dist/**/*'],
+      }),
+      new HtmlWebpackPlugin({
+        filename: 'home.html',
+        template: './src/home.html',
+      }),
+      new HtmlWebpackPlugin({
+        filename: 'detail.html',
+        template: './src/detail.html',
+      }),
+      new MiniCssExtractPlugin({
+        filename: '[name].css',
+        chunkFilename: '[id].css',
+      }),
+    ],
+    optimization: {},
+    resolve: {
+      modules: ['node_modules'],
+      extensions: ['.js', '.json', '.jsx', '.css'],
+    },
+  };
+
+  // if(options.mode == 'development') {
+
+  // }else if(options.mode == 'production') {
+  //   config = {
+  //     ...config,
+  //     mode : 'production',
+  //     output: {
+  //       path: path.resolve(__dirname, 'dist/nuguya/resources/'),
+  //       filename: '[name].js',
+  //       chunkFilename: '[name]-chunk.js',
+  //       publicPath: '/nuguya/resources/',
+  //     },
+  //   }
+  // }
+
+  return config;
 };
